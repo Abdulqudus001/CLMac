22515ba87971a0b772d8052c312e70eeb880762b
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-03-20 17:01:31 +0100

    Added reports section

--- scripts/reports.js
+++ scripts/reports.js
@@ -1,5 +1,40 @@
 const reports = document.querySelector('.report-stats');
 
+let reportTarget, iconSrc, reportIcon;
+
 reports.addEventListener('mouseover', e => {
-  console.log(e);
+  if (e.target.classList[0] != 'report-stats') {
+    reportTarget = getParentLink(e.target);
+  }
+  if (reportTarget) {
+    reportIcon = reportTarget.children[0].children[0];
+    iconSrc = getIconName(reportIcon.src);
+    reportIcon.src = `./assets/icons/${getDarkIconName(iconSrc)}`;
+  }
 });
+
+reports.addEventListener('mouseout', e => {
+  if (reportTarget) {
+    reportIcon.src = `./assets/icons/${iconSrc}`;
+    iconSrc = '';
+  }
+})
+
+const getParentLink = e => {
+  if (e.parentElement) {
+    const nodeName = e.parentElement.nodeName;
+    if (nodeName == 'A') {
+      return e.parentNode;
+    } else {
+      return getParentLink(e.parentNode);
+    }
+  }
+}
+
+const getIconName = str => {
+  return str.split('/')[str.split('/').length - 1];
+}
+
+const getDarkIconName = str => {
+  return str.split('.')[0] + '-white.svg';
+}
